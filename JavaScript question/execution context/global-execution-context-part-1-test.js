0e43d17b002b3c7799491123fc3b7f491be16dc6
moinul <moinulibr@gmail.com>
2026-04-03 12:40:48 +0600

    global and functional execution context

--- JavaScript question/execution context/global-execution-context-part-1-test.js
+++ JavaScript question/execution context/global-execution-context-part-1-test.js
@@ -3,3 +3,27 @@ let name   = 'Tom';
 function sayName (){
     console.log(this.name);
 }
+
+
+//------------------------------------------------------------
+
+    console.log('starting execution context - global execution context');
+    let a = 5; //creating phase of global execution context
+    function testMe() {
+        console.log("Inside testMe function - global execution context");
+        var b = 10;
+        var users = {
+            name : 'John',
+            age : 30,
+            country: "Bangladesh"
+        }
+        function testAgainInsideOfTestMe(){
+            console.log("Inside testAgainInsideOfTestMe function - global execution context");
+            console.log("existing testAgainInsideOfTestMe function - global execution context");
+        }
+        testAgainInsideOfTestMe();
+        console.log("existing testMe function - global execution context");
+    }
+
+    testMe();//creating phase of global execution context
+    console.log("existing global execution context");
